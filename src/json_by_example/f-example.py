83ed177d876c1f20ea46e4e4989b0bd9b3f4db3a
rewan999 <105081470+rewan999@users.noreply.github.com>
2025-07-24 21:06:21 +0300

    check it

--- src/json_by_example/f-example.py
+++ src/json_by_example/f-example.py
@@ -42,6 +42,9 @@ Use the examples in the previous files (module)  to Decode and Encode the above 
 You can see the Python object structure below.
 Try to debug your result so you can inspect and see how dictionary is format from the JSON object String 
 '''
+
+import json
+from datetime import datetime
 class Signal():
     def __init__(self, _place:str, _stamp:datetime, _samples:list[complex]):
         self._place = _place
@@ -51,19 +54,18 @@ class Signal():
     @property
     def place(self):
         return self._place
-
     @property
     def stamp(self):
-        return self.stamp
+        #there is an error here was missing _ befor stamp  
+        return self._stamp
 
     @property
     def samples(self):
-        return self._samples
+        return self._sample
 
 
 
-import json
-from datetime import datetime
+
 
 data =  """{
     "signal": {
@@ -97,5 +99,51 @@ data =  """{
     }
 }"""
 
-
+class DataEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, complex):
+            return  { 
+                     "__class":"complex",
+                     "real": obj.real,
+                     "imag": obj.imag
+                    }
+        elif(isinstance(obj, datetime)):
+            return {
+                    "__class": "datetime",
+                       "y": obj.year,
+                       "month": obj.month,
+                       "d":obj.day,
+                       "h": obj.hour,
+                       "minute": obj.minute,
+                       "s": obj.second
+                      }
         
+        return super().default(obj)
+    
+class DataDecoder(json.JSONDecoder):
+    def __init__(self):
+        json.JSONDecoder.__init__(self, object_hook=DataDecoder.from_dict)
+
+    @staticmethod
+    def from_dict(d):
+        if d.get("__class") == "complex":
+            return complex(d.get("real"), d.get("imag"))
+        elif d.get("__class") == "datetime":
+            return datetime(d["y"],d["month"],d["d"],d["h"],d["minute"],d["s"])
+                
+        return d 
+
+
+if __name__=='__main__':
+    d=json.loads(data,cls=DataDecoder)
+    print(d)
+    e=json.dumps(d,cls=DataEncoder,indent=4)
+    print(e)
+    place=d["signal"]["place"]
+    stamp=d["signal"]["stamp"]
+    samples=d["signal"]["samples:"]
+    
+    s=Signal(place,stamp,samples)
+    print(s.place)
+    print(s.stamp)
+    print(s.samples)
--- src/json_by_example/g-example.py
+++ src/json_by_example/g-example.py
@@ -21,3 +21,51 @@ data = """{
         }
     }
 }"""
+class DataEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if(isinstance(obj, date)):
+            return {
+                    "__class": "date",
+                       "y": obj.year,
+                       "month": obj.month,
+                       "d":obj.day,
+                      }
+        elif(isinstance(obj, timedelta)):
+            return {
+                       "__class": "timedelta",
+                       "days":obj.days,
+                       "seconds":obj.seconds,
+                       "microseconds":obj.microseconds                 
+                      }
+        elif(isinstance(obj, timezone)):
+                return {
+                       "__class": "timezone",
+                        "offset": obj.utcoffset(None)
+                      }
+
+        return super().default(obj)
+
+          
+        
+
+
+class DataDecoder(json.JSONDecoder):
+    def __init__(self):
+          json.JSONDecoder.__init__(self, object_hook=DataDecoder.from_dict)
+
+    @staticmethod
+    def from_dict(d):
+        if d.get("__class") == "date":
+            return date(d["y"],d["month"],d["d"])
+        elif d.get("__class") == "timedelta":
+            return timedelta(d["days"],d["seconds"],d["microseconds"])
+        elif d.get("__class") == "timezone":
+            return timezone(timedelta(seconds=d["offset"]))  
+        return d 
+          
+                
+if __name__=="__main__":
+     d=json.loads(data,cls=DataDecoder)
+     print(d)
+     e=json.dumps(d,cls=DataEncoder, indent=4)
+     print(e)
